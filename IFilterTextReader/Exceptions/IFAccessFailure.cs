39b8bb31aa4dbf1f9ebbd6d10c676e0845f30f11
Kees <sicos2002@hotmail.com>
2014-11-11 16:05:28 +0100

    Restored exception classes

---
+++ IFilterTextReader/Exceptions/IFAccessFailure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace IFilterTextReader.Exceptions
+{
+    /// <summary>
+    /// Raised when a file or IFilter cannot be accessed 
+    /// </summary>
+    [Serializable]
+    public class IFAccesFailure : Exception
+    {
+        protected IFAccesFailure(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public IFAccesFailure() { }
+
+        public IFAccesFailure(string message) : base(message) { }
+
+        public IFAccesFailure(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ IFilterTextReader/Exceptions/IFFilterNotFound.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace IFilterTextReader.Exceptions
+{
+    /// <summary>
+    /// Raised when there is not IFilter installed for a file
+    /// </summary>
+    [Serializable]
+    public class IFFilterNotFound : Exception
+    {
+        protected IFFilterNotFound(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public IFFilterNotFound() { }
+
+        public IFFilterNotFound(string message) : base(message) { }
+
+        public IFFilterNotFound(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
