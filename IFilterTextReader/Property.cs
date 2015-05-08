4868029743e600cc310236ca3835999f4b0ed9fc
sicos1977 <sicos2002@hotmail.com>
2015-05-08 20:17:09 +0200

    Cleaned up some code and added new meta data properties

--- IFilterTextReader/Property.cs
+++ IFilterTextReader/Property.cs
@@ -1,4 +1,21 @@
 ﻿using System;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+/*
+   Copyright 2013-2015 Kees van Spelde
+
+   Licensed under The Code Project Open License (CPOL) 1.02;
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.codeproject.com/info/cpol10.aspx
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 namespace IFilterTextReader
 {
@@ -47,7 +64,7 @@ namespace IFilterTextReader
         public string FriendlyName { get; private set; }
 
         /// <summary>
-        /// The <see cref="MetadataPropertyType">type</see> of the property
+        /// The <see cref="PropertyType">type</see> of the property
         /// </summary>
         public PropertyType Type { get; private set; }
 
--- IFilterTextViewer/MainForm.cs
+++ IFilterTextViewer/MainForm.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.Windows.Forms;
 
 /*
-   Copyright 2014 Kees van Spelde
+   Copyright 2013-2015 Kees van Spelde
 
    Licensed under The Code Project Open License (CPOL) 1.02;
    you may not use this file except in compliance with the License.
